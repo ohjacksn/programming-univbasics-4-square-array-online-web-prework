03529e8913c8e4d192dc591d169ea7f45f855e68
Jackson Gray Morgan <jacksonmo@me.com>
2020-01-07 02:28:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  result = [ ]
+    while array[counter] do
+      result.push array[counter]** 2
+      counter += 1
+    end
+    return result
 end
